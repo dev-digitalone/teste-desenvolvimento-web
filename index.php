d3b3d2fed58a757c5af2e27e2cca832c54c82df6
ivanluizjr <ivanluizjr@hotmail.com>
2020-06-23 15:15:02 -0300

    criando conexão com banco de dados

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head lang="pt-br">
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+<h2>Autenticação do Sistema</h2><hr>
+
+<?php include_once 'resource/Database.php'?>
+</body>
+</html>
