a29075290f8a42c8e947cdd8b68c0140b8bba95d
ivanluizjr <ivanluizjr@hotmail.com>
2020-06-24 22:27:23 -0300

    criando layout do sistema

--- login.php
+++ login.php
@@ -52,23 +52,74 @@ if(isset($_POST['entrarBtn'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina de Login</title>
-</head>
-<body>
 
-<h3>Formulário de Login</h3>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <link href="css/login.css" rel="stylesheet">
+</head>
+<body class="text-center">
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <h2><a class="navbar-brand" href="#">DigitalOne Post</a></h2>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="signup.php">Cadastro <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    </div>
+</nav>
 
 <?php if(isset($result)) echo $result; ?>
 <?php if(!empty($form_errors)) echo show_errors($form_errors);?>
-<form method="post" action="">
-    <table>
-        <tr><td>Usuário</td> <td><input type="text" name="username" value=""></td></tr>
-        <tr><td>Senha</td> <td><input type="password" name="password" value=""></td></tr>
-        <tr><td></td> <td><input type="submit" value="Entrar" name="entrarBtn"></td></tr>
-    </table>
-    <p><a href="forgot_password.php">Resetar Senha?</a></p>
+<form class="form-signin" method="post" action="">
+  <h1 class="h3 mb-3 font-weight-normal">Entre com Usuário e Senha</h1>
+  <label for="inputEmail" class="sr-only">Usuário</label>
+  <input type="text" name="username" id="inputUsuario" class="form-control" placeholder="Usuário" required autofocus>
+  <label for="inputPassword" class="sr-only">Senha</label>
+  <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Relembrar dados
+    </label>
+  </div>
+  <button class="btn btn-lg btn-secondary btn-block" name="entrarBtn" type="submit">Entrar</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
 </form>
-
-<p><a href="index.php">Voltar</a></p>
     
 </body>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
--- signup.php
+++ signup.php
@@ -1,69 +1,151 @@
-<?php 
-    include_once 'resource/Database.php';
-    include_once 'resource/utilities.php';
+<?php
+//add our database connection script
+include_once 'resource/Database.php';
+include_once 'resource/utilities.php';
 
-    if(isset($_POST['enviarBtn'])){
+//process the form
+if(isset($_POST['enviarBtn'])){
+    //initialize an array to store any error message from the form
+    $form_errors = array();
 
-        $form_errors = array();
+    //Form validation
+    $required_fields = array('email', 'username', 'password');
 
-        $require_fields = array('email', 'username', 'password');
+    //call the function to check empty field and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
 
-        $form_errors = array_merge($form_errors, check_empty_fields($require_fields));
+    //Fields that requires checking for minimum length
+    $fields_to_check_length = array('username' => 4, 'password' => 6);
 
-        $fields_to_checck_length = array('username' => 4, 'password' => 6);
-        
-        $form_errors = array_merge($form_errors, check_min_length($fields_to_checck_length));
+    //call the function to check minimum required length and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
 
-        $form_errors = array_merge($form_errors, check_email($_POST));
-    }
+    //email validation / merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_email($_POST));
 
+    //check if error array is empty, if yes process form data and insert record
     if(empty($form_errors)){
-        
+        //collect form data and store in variables
         $email = $_POST['email'];
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        //hashing the password
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
         try{
-            $sqlInsert = "INSERT INTO users (name, email, password, join_date)
-                      VALUES (:username, :email, :password, now())";
 
+            //create SQL insert statement
+            $sqlInsert = "INSERT INTO users (username, email, password, join_date)
+              VALUES (:username, :email, :password, now())";
+
+            //use PDO prepared to sanitize data
             $statement = $db->prepare($sqlInsert);
-            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));  
-        
+
+            //add the data into the database
+            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+            //check if one new row was created
             if($statement->rowCount() == 1){
-                $result = "<p style='padding: 20px; color: green'>Registrado com Sucesso!</p>";
+                $result = "<p style='padding:20px; border: 1px solid gray; color: green;'> Registration Successful</p>";
             }
-        }catch(PDOException $ex){
-            $result = "<p style='padding: 20px; color: red'>Ocorreu um erro: ".$ex->getMessage()."</p>";
-        }           
+        }catch (PDOException $ex){
+            $result = "<p style='padding:20px; border: 1px solid gray; color: red;'> An error occurred: ".$ex->getMessage()."</p>";
+        }
     }
+    else{
+        if(count($form_errors) == 1){
+            $result = "<p style='color: red;'> There was 1 error in the form<br>";
+        }else{
+            $result = "<p style='color: red;'> There were " .count($form_errors). " errors in the form <br>";
+        }
+    }
+
+}
+
 ?>
-
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
+<html>
+<head lang="en">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina de Cadastro</title>
-</head>
-<body>
+    <title>Register Page</title>
 
-<h3>Formulário de Cadastro</h3>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <link href="css/login.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <h2><a class="navbar-brand" href="#">DigitalOne Post</a></h2>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="signup.php">Cadastro <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    </div>
+</nav>
 
 <?php if(isset($result)) echo $result; ?>
-<?php if(!empty($form_errors)) echo show_errors($form_errors);?>
-<form method="post" action="">
+<?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
+<form class="form-signin" method="post" action="">
+  <h1 class="h3 mb-3 font-weight-normal">Cadastro de Usuário</h1>
+  <label for="inputEmail" class="sr-only">Email</label>
+  <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+  <label for="inputUsuario" class="sr-only">Usuario</label>
+  <input type="text" name="username" id="inputUsername" class="form-control" placeholder="Usuário" required>
+  <label for="inputSenha" class="sr-only">Senha</label>
+  <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+  <div class="checkbox mb-3">
+  </div>
+  <button class="btn btn-lg btn-secondary btn-block" name="enviarBtn" type="submit">Enviar</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+</form>
+    
+</body>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<!--<form method="post" action="">
     <table>
-        <tr><td>Email</td> <td><input type="text" value="" name="email"></td></tr>
-        <tr><td>Usuário</td> <td><input type="text" value="" name="username"></td></tr>
-        <tr><td>Senha</td> <td><input type="password" value="" name="password"></td></tr>
-        <tr><td></td> <td><input type="submit" value="Enviar" name="enviarBtn"></td></tr>
+        <tr><td>Email:</td> <td><input type="text" value="" name="email"></td></tr>
+        <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
+        <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
+        <tr><td></td><td><input style="float: right;" type="submit" name="enviarBtn" value="Signup"></td></tr>
     </table>
 </form>
-
-<p><a href="index.php">Voltar</a></p>
-    
+<p><a href="index.php">Back</a> </p>-->
 </body>
 </html>
