9947d141675a069c682cf4b2ebde97a3a65e1a6f
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-17 19:39:24 -0300

    configuração da conexão com o banco

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const app = require("./src/app");
 
+const conn = require("./src/config/db/conn");
+
 const port = 4000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-});
+conn.sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}.`);
+        });
+    })
+    .catch((err) => console.log(err));
