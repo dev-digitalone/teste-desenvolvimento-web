975521d137bdf17eb1d11cb4a45dc143bff3f170
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-17 19:40:11 -0300

    model de usuários

---
+++ backend/src/models/User.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../config/db/conn");
+
+const User = db.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = User;
