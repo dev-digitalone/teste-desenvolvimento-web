ac1033bdbe2a12f0732366d66a357626af167b4a
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-17 19:34:07 -0300

    configuração do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
