c3367eae9b9424c511e0aaebb99a0d8ec8c0215e
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-17 21:17:54 -0300

    pegar usuário por id

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,4 +10,24 @@ module.exports = class UserController {
 
         res.status(200).json({ users: userDb });
     }
+
+    static async getUserById(req, res) {
+        const id = req.params.id;
+
+        const userDb = await User.findOne({
+            where: {
+                id: id,
+            },
+            attributes: {
+                exclude: ["password"],
+            },
+        });
+
+        if (!userDb) {
+            res.status(404).json({ msg: "Usuário não encontrado!" });
+            return;
+        }
+
+        res.status(200).json({ user: userDb });
+    }
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const UserController = require("../controllers/userController");
 
 router.get("/", UserController.getUsers);
+router.get("/:id", UserController.getUserById);
 
 module.exports = router;
