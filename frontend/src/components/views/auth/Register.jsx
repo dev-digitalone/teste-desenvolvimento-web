c37d7687927e0d2efbdd52736b1dbe6e3149feb9
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-19 20:10:22 -0300

    props para o input e adicionado nova regra ao lint

--- frontend/src/components/views/auth/Register.jsx
+++ frontend/src/components/views/auth/Register.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
-import Inputs from '../../form/inputs';
-
-export default function Register() {
+export default function Register({
+    label,
+    isRequired,
+    type,
+    placeholder,
+    value,
+    validateMsg,
+}) {
     return (
         <section>
-            <Inputs />
+            <Form.Label>{label}</Form.Label>
+            <Form.Control
+                required={isRequired}
+                type={type}
+                placeholder={placeholder}
+                defaultValue={value}
+            />
+            <Form.Control.Feedback>{validateMsg}</Form.Control.Feedback>
         </section>
     );
 }
