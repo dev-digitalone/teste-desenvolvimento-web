58a48a9a660a87e0408336d4894fc48d5ee96865
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-19 01:46:41 -0300

    layout inicial do footer da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/template/Footer';
 import Header from './components/template/Header';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
         </div>
     );
 }
