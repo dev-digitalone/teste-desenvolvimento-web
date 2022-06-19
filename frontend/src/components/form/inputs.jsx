b682b67ef3f3923c7a4a7264f2c3ac4184634f29
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-19 20:15:03 -0300

    correção na criação do component input

--- frontend/src/components/form/inputs.jsx
+++ frontend/src/components/form/inputs.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
-export default function Input() {
-    return <h1>Inputs</h1>;
+export default function Input({
+    label,
+    isRequired,
+    type,
+    placeholder,
+    value,
+    validateMsg,
+}) {
+    return (
+        <>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control
+                required={isRequired}
+                type={type}
+                placeholder={placeholder}
+                defaultValue={value}
+            />
+            <Form.Control.Feedback>{validateMsg}</Form.Control.Feedback>
+        </>
+    );
 }
--- frontend/src/components/views/auth/Register.jsx
+++ frontend/src/components/views/auth/Register.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
 
-export default function Register({
-    label,
-    isRequired,
-    type,
-    placeholder,
-    value,
-    validateMsg,
-}) {
+import Inputs from '../../form/inputs';
+
+export default function Register() {
     return (
         <section>
-            <Form.Label>{label}</Form.Label>
-            <Form.Control
-                required={isRequired}
-                type={type}
-                placeholder={placeholder}
-                defaultValue={value}
-            />
-            <Form.Control.Feedback>{validateMsg}</Form.Control.Feedback>
+            <Inputs />
         </section>
     );
 }
