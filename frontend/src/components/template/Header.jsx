6b3a13a6e55a95768a099d8710f0e9f2cbe825d2
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-20 18:35:59 -0300

    navbar refatorada com react bootstrap

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -1,57 +1,26 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                {/* <button className="navbar-toggler" type="button">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="collapse navbar-collapse d-flex justify-content-between">
-                    <a className="navbar-brand" href="/">
-                        Teste-DigitalOne
-                    </a>
-                    {/* Futura funcinalidade ? */}
-                    {/* <form
-                        className="d-flex justify-content-center"
-                        role="search"
-                    >
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button
-                            className="btn btn-outline-success"
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Container className="m-0 mx-3" style={{ maxWidth: '100%' }}>
+                <Navbar.Brand className="navbar-brand" href="/">
+                    Teste-DigitalOne
+                </Navbar.Brand>
+                <Nav className="d-flex justify-content-center">
+                    <Link to="/entrar">
+                        <Button
+                            className="mx-2"
+                            variant="outline-primary"
                             type="submit"
                         >
-                            Search
-                        </button>
-                    </form> */}
-                    <div className="d-flex justify-content-center">
-                        <div>
-                            <Link to="/entrar">
-                                <button
-                                    className="btn btn-outline-primary mx-2"
-                                    type="submit"
-                                >
-                                    Entrar
-                                </button>
-                            </Link>
-                            <Link to="/registrar">
-                                <button
-                                    className="btn btn-outline-primary mx-2"
-                                    type="submit"
-                                >
-                                    Cadastrar
-                                </button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
+                            Entrar
+                        </Button>
+                    </Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 }
