79e29ab2b9476ae28649f560a3e8360cc4c6ce9b
Guilherme <guilherme.s.goncalves@outlook.com>
2022-06-21 18:17:09 -0300

    layout do componente de recuperar senha

--- frontend/src/components/views/auth/ForgotPassword.jsx
+++ frontend/src/components/views/auth/ForgotPassword.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
+import { Card, Form, Button } from 'react-bootstrap';
+
+import Inputs from '../../form/Inputs';
+import styles from '../../form/Form.module.css';
+
 export default function ForgotPassword() {
-    return <h1>Esqueci minha senha</h1>;
+    return (
+        <section>
+            <Card className={styles.form_container}>
+                <div>
+                    <h1>Recuperar senha</h1>
+                </div>
+                <Form onSubmit="">
+                    <Inputs
+                        label="E-mail:"
+                        placeholder="Digite o seu email"
+                        type="email"
+                        name="email"
+                        isRequired="required"
+                    />
+
+                    <div className="d-grid gap-2 col-12 mx-auto mt-5">
+                        <Button type="submit">Enviar</Button>
+                    </div>
+                </Form>
+            </Card>
+        </section>
+    );
 }
