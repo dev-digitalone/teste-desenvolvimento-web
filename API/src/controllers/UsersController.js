bb6d902167d1cc75f93c59f0a7f3932c702b8a39
carlos <carloslonghi.cl@gmail.com>
2023-05-04 19:03:00 -0300

    feat: create Users Controller and Routes

---
+++ API/src/controllers/UsersController.js
@@ -0,0 +1,10 @@
+class UsersController {
+
+  create(request, response) {
+    const { name, email, password } = request.body
+
+    response.status(201).json({ name, email, password })
+  }
+}
+
+module.exports = UsersController
