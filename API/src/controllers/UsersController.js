42e55e374ae3eabd076bc3771f4b7a59954c7e20
carlos <carloslonghi.cl@gmail.com>
2023-05-04 22:17:35 -0300

    feat: create exception handling in UsersController

--- API/src/controllers/UsersController.js
+++ API/src/controllers/UsersController.js
@@ -1,7 +1,19 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
 
   create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name){
+      throw new AppError("Nome obrigatório!")
+    }    
+    if(!email){
+      throw new AppError("Email obrigatório!")
+    }
+    if(!password){
+      throw new AppError("Senha obrigatória!")
+    }
 
     response.status(201).json({ name, email, password })
   }
