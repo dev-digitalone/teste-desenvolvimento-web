31660da72d1b361c4de3387af300ce2541161e23
carlos <carloslonghi.cl@gmail.com>
2023-05-08 16:16:43 -0300

    style: create global and theme styles

---
+++ WebApp/src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#0D0C0F',
+    BACKGROUND_800: '#1C1B1E',
+    BACKGROUND_700: '#262529',
+
+    WHITE: '#F4EDE8',
+    BLUE: '#307bff',
+
+    GRAY_100: '#cdc6c6',
+    GRAY_200: '#b5b1ac',
+    GRAY_300: '#968a8a',
+
+    RED: '#FF0000',
+    GREEN: '#028800'
+  }
+}
