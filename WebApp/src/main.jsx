e5d86476a7ceba6e13a19383b0243467ec3d777a
carlos <carloslonghi.cl@gmail.com>
2023-05-09 23:52:23 -0300

    last commit

--- WebApp/src/main.jsx
+++ WebApp/src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
+import { AuthProvider } from './hooks/auth'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <AuthProvider>
+        <Routes/>
+      </AuthProvider>
+    </ThemeProvider>
+  
   </React.StrictMode>,
 )
